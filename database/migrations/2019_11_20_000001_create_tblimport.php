39dd756a03c7384d54e2096c720a6cf96dfcaab8
Anthony S. A <asalazar0592@gmail.com>
2019-11-20 17:27:21 -0600

    Migratios

---
+++ database/migrations/2019_11_20_000001_create_tblimport.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTBLimport extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tblimport', function (Blueprint $table) {
+            $table->increments('tblimportid');
+            $table->year('tblimportbirth');
+            $table->string('tblimportgender',6);
+            $table->string('tblimportethnicity',45);
+            $table->string('tblimportchildfirst',45);
+            $table->string('tblimportcount',3);
+            $table->string('tblimportrank',3);
+        });
+    }
+    
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tblimport');
+    }
+}
